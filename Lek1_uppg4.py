565984735b7f2c44c4ae7413680702697cdd3593
edgbou <edgbou@gmail.com>
2025-11-25 18:57:25 +0100

    second commit

--- Lek1_uppg4.py
+++ Lek1_uppg4.py
@@ -1,1 +1,3 @@
 #test på hur man sätter upp och uppdaterar ett repository på github
+
+#Bekräftelse på att repot är överfört till wsl
---
+++ Lek1_uppg5.py
@@ -0,0 +1,2 @@
+#Test 2 för att skapa en fil och lägga upp den i repot
+
