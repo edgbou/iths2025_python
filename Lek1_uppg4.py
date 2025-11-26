f83d48b8480c3d4b9593be335c9f305412cd8c34
edgbou <edgbou@gmail.com>
2025-11-26 15:56:22 +0100

    Slutförd uppgift

--- Lek1_uppg4.py
+++ Lek1_uppg4.py
@@ -1,5 +1,14 @@
-#test på hur man sätter upp och uppdaterar ett repository på github
+target_ip = "10.5.20.234"
+open_ports = [22, 80, 44]
+vulnerable = True
 
-#Bekräftelse på att repot är överfört till wsl
+print("The target IP is",target_ip)
+print(type(target_ip))
+print("\n")
 
-#test igen och igen
+print("The open ports on the network are", open_ports)
+print(type(open_ports))
+print("\n")
+
+print("Vulnerable Network:", vulnerable)
+print(type(vulnerable))
