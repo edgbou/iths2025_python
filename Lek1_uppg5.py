74cec7c4f180bfb0ad266e37d6b60c359ed16601
edgbou <edgbou@gmail.com>
2025-11-26 16:41:06 +0100

    Slutförd uppgift

--- Lek1_uppg5.py
+++ Lek1_uppg5.py
@@ -1,2 +1,8 @@
-#Test 2 för att skapa en fil och lägga upp den i repot
+scanned_hosts = 50
+found_vulnerable = 8
 
+vulnerable_machine = found_vulnerable / scanned_hosts * 100
+
+print(vulnerable_machine > 10 )
+
+print(f"{vulnerable_machine:g}% of the scanned hosts are vulnerable")
